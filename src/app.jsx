10f34cb278c94098a9f1f2f0ed776465540e636f
AE <austence@sbcglobal.net>
2019-04-15 14:12:10 -0700

    adds package-lock.json and .prettierrc files.  Also reformats gradient in index.html.  Also adds jumbotron and styling to app.jsx.

--- src/app.jsx
+++ src/app.jsx
@@ -3,7 +3,16 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div className='App'></div>
+      <div className='App fluid'>
+
+        <div className='jumbotron p-2 m-3 bg-white text-center border border-black sticky-top'>
+          <h1 className='display-3'>San Diego Top Spots</h1>
+          <hr className='mx-5' />
+          <p className='lead font-italic'>A list of the top 30 places to see in San Diego, California.</p>
+          {/* <hr /> */}
+        </div>
+
+      </div>
     );
   }
 }
