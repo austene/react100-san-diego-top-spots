373b5ec0f80744fb7c2b3675a2c8975feb4d52b8
AE <austence@sbcglobal.net>
2019-04-16 11:03:10 -0700

    adds code in Topspots.jsx to open map in new tab.  Also adds code for security when opening the tap.

--- src/Topspot.jsx
+++ src/Topspot.jsx
@@ -5,7 +5,11 @@ export default props => (
   <div className='well bg-white'>
     <h4>{props.name}</h4>
     <p>{props.description}</p>
-    <a className='btn btn-primary' href={ `https://www.google.com/maps?q=${props.location[0]},${props.location[1]}` }>Map</a>
+    <a
+      className='btn btn-primary'
+      target='_blank'
+      rel='noopener noreferrer'
+      href={ `https://www.google.com/maps?q=${props.location[0]},${props.location[1]}` }>Map</a>
     <p />
   </div>
 );
