a59a53e5499ce8cc630b5ccc10c3a8d28f9b9396
AE <austence@sbcglobal.net>
2019-04-16 10:44:19 -0700

    adds Topspot.jsx, adds styling to index.html for margins and wells, adds code to app.jsx to render buttons.

---
+++ src/Topspot.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export default props => (
+  <div className='well bg-white'>
+    <h4>{props.name}</h4>
+    <p>{props.description}</p>
+    <a className='btn btn-primary' href={ `https://www.google.com/maps?q=${props.location[0]},${props.location[1]}` }>Map</a>
+    <p />
+  </div>
+);
--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,43 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import Topspots from './Topspot';
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      topspots: []
+    };
+  }
+
+  componentWillMount() {
+    axios
+      .get('https://origin-top-spots-api.herokuapp.com/api/topspots')
+      .then(response => response.data)
+      .then(topspots => this.setState({ topspots }));
+  }
   render() {
     return (
       <div className='App fluid'>
 
-        <div className='jumbotron p-2 m-3 bg-white text-center border border-black sticky-top'>
+        <div className='jumbotron p-2 bg-white text-center border border-black sticky-top'>
           <h1 className='display-3'>San Diego Top Spots</h1>
           <hr className='mx-5' />
           <p className='lead font-italic'>A list of the top 30 places to see in San Diego, California.</p>
           {/* <hr /> */}
         </div>
+        {
+          this.state.topspots.map(topspot => (
+            <Topspots
+              key={ topspot.id }
+              name={ topspot.name }
+              description={ topspot.description }
+              location={ topspot.location }
+            />
+          ))
+        }
 
       </div>
     );
